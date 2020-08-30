a35c81d47c35416096bbf9d3f4e1a0dfbe182756
Juan Camilo Cardenas Gomez <juaccardenasgom@unal.edu.co>
2020-08-30 00:44:48 -0500

    chapter 2 done!

--- guessing_game/target/debug/guessing_game.d
+++ guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-/mnt/c/Users/juanc/Documents/learn-rust/guessing_game/target/debug/guessing_game: /mnt/c/Users/juanc/Documents/learn-rust/guessing_game/src/main.rs
+/mnt/c/Users/juanc/Documents/learn-rust/guessing_game/target/debug/guessing_game: /mnt/c/Users/juanc/Documents/learn-rust/guessing_game/src/main.rs build.rs
