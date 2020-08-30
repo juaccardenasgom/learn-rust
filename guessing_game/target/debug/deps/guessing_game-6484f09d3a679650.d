1f4c21af340ea760c4ffaa563f5aa1ee0fb64f82
Juan Camilo Cardenas Gomez <juaccardenasgom@unal.edu.co>
2020-08-30 00:47:56 -0500

    removed target folders

--- guessing_game/target/debug/deps/guessing_game-6484f09d3a679650.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/juanc/Documents/learn-rust/guessing_game/target/debug/deps/guessing_game-6484f09d3a679650: src/main.rs
-
-/mnt/c/Users/juanc/Documents/learn-rust/guessing_game/target/debug/deps/guessing_game-6484f09d3a679650.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/release/hello-cargo.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/juanc/Documents/learn-rust/hello-cargo/target/release/hello-cargo: /mnt/c/Users/juanc/Documents/learn-rust/hello-cargo/src/main.rs
