40982437eeb9ac0cbbf6d123f03e437afcb36d30
Juan Camilo Cardenas Gomez <juaccardenasgom@unal.edu.co>
2020-09-02 23:51:58 -0500

    branches and chapter 3 done!

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -11,4 +11,37 @@ fn main() {
     let number2 = if condition { 5 } else { 6 };
     println!("The value of number is: {}", number2);
     
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10,20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("The value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
